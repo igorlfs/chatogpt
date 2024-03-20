714558a4f4a81efb68de46619a4d40b2bc69b948
Igor <igorlfs@ufmg.br>
2024-03-20 00:06:02 -0300

    feat: basic chat functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rand::{thread_rng, Rng};
+
+const NUM_POSSIBLE_ANSWERS: i32 = 1;
+
+fn alternate_string_case(string: &str) -> String {
+    string
+        .char_indices()
+        .map(|(idx, c)| {
+            if idx % 2 == 0 {
+                c.to_lowercase().to_string()
+            } else {
+                c.to_uppercase().to_string()
+            }
+        })
+        .collect()
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+fn message_to_reply(message: &str) -> (i32, String) {
+    let reply_id = thread_rng().gen_range(0..=NUM_POSSIBLE_ANSWERS);
+    let reply = match reply_id {
+        0 => "Pong!".to_string(),
+        _ => alternate_string_case(message),
+    };
+    (reply_id, reply)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![message_to_reply])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/InputForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import { invoke } from "@tauri-apps/api";
+
+const InputForm = () => {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const [_, reply] = await invoke("message_to_reply", {
+            message: inputValue,
+        });
+        console.log("Submitted value:", reply);
+        setInputValue("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Type something..."
+            />
+            <button type="submit">Send</button>
+        </form>
+    );
+};
+
+export default InputForm;
