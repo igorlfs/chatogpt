e0e0a7c7eb19ae3f9cb058b05322de5b3960c8de
Igor <igorlfs@ufmg.br>
2024-05-01 01:05:06 -0300

    feat: match email regex

--- src-tauri/src/strings.rs
+++ src-tauri/src/strings.rs
@@ -1,4 +1,18 @@
+use regex::Regex;
 use unicode_segmentation::UnicodeSegmentation;
+
+fn is_email_address(string: &str) -> bool {
+    let email_regex = Regex::new(r"^[^\s@]+@[^\s@]+\.[^\s@]{2,}$").unwrap();
+    email_regex.is_match(string)
+}
+
+pub fn match_email_address(string: &str) -> String {
+    if is_email_address(string) {
+        format!("{string} 📩✅").to_string()
+    } else {
+        format!("{string} 📩❎").to_string()
+    }
+}
 
 pub fn alternate_string_case(string: &str) -> String {
     let graphemes = string.graphemes(true).collect::<Vec<&str>>();
@@ -17,28 +31,94 @@ pub fn alternate_string_case(string: &str) -> String {
 
 #[cfg(test)]
 mod test {
-    use super::alternate_string_case;
+    use super::{alternate_string_case, is_email_address};
 
     #[test]
-    fn test_happy_string() {
+    fn test_actual_email_address() {
+        let actual = is_email_address("user@domain.com");
+        assert!(actual);
+    }
+
+    #[test]
+    fn test_email_address_empty() {
+        let actual = is_email_address("");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_email_address_starts_with_spaces() {
+        let actual = is_email_address(" user@domain.com");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_email_address_ends_with_spaces() {
+        let actual = is_email_address("user@domain.com ");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_email_address_missing_domain() {
+        let actual = is_email_address("user@.com");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_email_address_missing_top_level_domain() {
+        let actual = is_email_address("user@domain");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_email_address_missing_user() {
+        let actual = is_email_address("@domain.com");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_email_address_spaces_in_domain() {
+        let actual = is_email_address("user@ domain.com");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_email_address_spaces_in_user() {
+        let actual = is_email_address("us er@domain.com");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_email_address_spaces_in_top_level_domain() {
+        let actual = is_email_address("user@domain.c om");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_email_address_top_level_domain_too_short() {
+        let actual = is_email_address("user@domain.c");
+        assert!(!actual);
+    }
+
+    #[test]
+    fn test_alternate_case_basic_string() {
         let string = alternate_string_case("simple");
         assert_eq!(string, "sImPlE");
     }
 
     #[test]
-    fn test_string_with_spaces() {
+    fn test_alternate_case_with_spaces() {
         let string = alternate_string_case("hi sir");
         assert_eq!(string, "hI SiR");
     }
 
     #[test]
-    fn test_string_with_special_characters() {
+    fn test_alternate_case_with_special_characters() {
         let string = alternate_string_case("têstãç");
         assert_eq!(string, "tÊsTãÇ");
     }
 
     #[test]
-    fn test_string_with_emojis() {
+    fn test_alternate_case_with_emojis() {
         let string = alternate_string_case("🤓");
         assert_eq!(string, "🤓");
     }
